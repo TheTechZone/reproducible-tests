804ed5e1351a0a85d70f7a6c7b7fb2821051e516
Cerenia <christelle.gloor@inf.ethz.ch>
2024-12-11 15:00:38 +0000

    only uniquely pairwise compare

--- compare_local_mudkip_apks.py
+++ compare_local_mudkip_apks.py
@@ -8,6 +8,11 @@ def apk_filepath(bundle_filepath, apk_name):
     return os.path.join("/", *partial, "apks", "splits", apk_name)
 
 
+def get_folder_run_from_apk_filepath(path):
+    partial = path.split("/")
+    return f"{partial[-3]}_{partial[-2]}"
+
+
 def run_apkdiff(path1, path2):
     # for this quick and dirty test we simply used the copied apkdiff instead of fetching it every time
     python = local["python"]
@@ -19,7 +24,7 @@ def run_apkdiff(path1, path2):
 
 DIRECTORY_OF_INTEREST = "local_builds_mudkip"
 
-INCLUDE_ALL_RUNS = ["no_disorderfs", "disorderfs_no_sort"]
+INCLUDE_ALL_RUNS = ["bare", "dfs_no_sort"]
 APKS_OF_INTEREST = ["base-master.apk", "base-arm64_v8a.apk", "base-xxhdpi.apk"]
 
 unique_bundle_filepaths = []
@@ -34,18 +39,16 @@ for path, _, files in os.walk(os.getcwd()):
                 elif "01" in path:
                     unique_bundle_filepaths.append(filepath)
 
-print(unique_bundle_filepaths)
+#print(unique_bundle_filepaths)
 
 results = []
 for apk in APKS_OF_INTEREST:
-    for i in range(0, len(unique_bundle_filepaths)):
-        for j in range(0, len(unique_bundle_filepaths)):
+    for i in range(0, int(len(unique_bundle_filepaths))):
+        for j in range(0, i):
             if i != j:
                 result = run_apkdiff(apk_filepath(unique_bundle_filepaths[i], apk), apk_filepath(unique_bundle_filepaths[j], apk))
-                print(i, j, f" :{result}")
+                #print(i, j, f" :{result}")
                 results.append(result)
-
-print(results)
-
-
-
+                if result:
+                    ### Spit out any result that matched in a readable form
+                    print(f"{apk} matched: {get_folder_run_from_apk_filepath(unique_bundle_filepaths[i])} - {get_folder_run_from_apk_filepath(unique_bundle_filepaths[j])}")
