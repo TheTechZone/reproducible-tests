c6d7151cf9da355d5cdbc3fb03c7bca07ef44e73
Adrian Cucoș <dielorg@gmail.com>
2024-12-14 19:27:22 +0100

    ok, this is bad :c

--- strip_timestamps.py
+++ strip_timestamps.py
@@ -1,12 +1,12 @@
-#!python3
+#!/usr/bin/env python3
 
 import sys
 import re
 
 # timing & progress indicators
-timing_p = r'\d+(\.\d+)?s'
-download_speed_p = r'\(\d+ kB/s\)'
-remote_repository_progress_p = r'\[=* *\] \d+%'
+timing_p = r"\d+(\.\d+)?s"
+download_speed_p = r"\(\d+ kB/s\)"
+remote_repository_progress_p = r"\[=* *\] \d+%"
 
 patterns = [timing_p, download_speed_p, remote_repository_progress_p]
 
@@ -15,38 +15,39 @@ def main():
     if len(sys.argv) != 2:
         print("Usage: ./strip_timestamp <path/to/log.txt>")
         sys.exit(1)
-    
+
     path = sys.argv[1]
     print(f"Stripping timestamps from: {path}...")
-    
-    with open(path, 'r') as f:
+
+    with open(path, "r") as f:
         lines = f.readlines()
-      
+
     newlines = []
     for line in lines:
         newline = line.strip()
-        #print(f"Oldline:{newline}")
-        if line[0] == '#':
+        # print(f"Oldline:{newline}")
+        if line[0] == "#":
             parts = line.split(" ")
             try:
                 float(parts[1])
-                del[parts[1]]
+                del [parts[1]]
                 newline = " ".join(parts)
             except ValueError:
                 pass
         for p in patterns:
             newline = re.sub(p, "", newline)
-        #print(f"Newline:{newline}")
+        # print(f"Newline:{newline}")
         newlines.append(newline)
-    
+
     path_parts = path.split("/")
-    del(path_parts[-1])
+    del path_parts[-1]
     newpath = "/".join(path_parts) + "/log_no_time.txt"
-    
+
     with open(newpath, "w") as f:
         f.writelines(newlines)
-        
+
     print(f"Result saved in: {newpath}")
-    
+
+
 if __name__ == "__main__":
     main()
