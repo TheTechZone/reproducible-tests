4377489cad111d36c5e23d90c8afb96b50190ba0
Cerenia <christelle.gloor@inf.ethz.ch>
2024-12-15 21:08:29 +0000

    added playstore apks to data

--- compare_apks.py
+++ compare_apks.py
@@ -11,7 +11,11 @@ def apk_filepath(bundle_filepath, apk_name):
 
 def get_folder_run_from_apk_filepath(path):
     partial = path.split("/")
-    return f"{partial[-3]}_{partial[-2]}"
+    if "dfs_no_sort" in path: # TODO: Hacky
+        return f"{partial[-3]}_{partial[-2]}"
+    else:
+        return partial[-3]
+
 
 
 def run_apkdiff(path1, path2):
@@ -23,7 +27,7 @@ def run_apkdiff(path1, path2):
     return rc == 0
 
 
-DIRECTORIES_OF_INTEREST = ["local_builds_mudkip", "Aditz", "Andrea"]
+DIRECTORIES_OF_INTEREST = ["local_builds_mudkip", "Aditz", "Andrea", "device"]
 
 INCLUDE_ALL_RUNS = ["bare", "dfs_no_sort"]
 APKS_OF_INTEREST = ["base-master.apk", "base-arm64_v8a.apk", "base-xxhdpi.apk"]
@@ -40,21 +44,33 @@ for path, _, files in os.walk(os.getcwd()):
                 elif "01" in path:
                     unique_bundle_filepaths.append(filepath)
 
+# TODO: Hacky
+unique_bundle_filepaths.append("data/device")
 # print(unique_bundle_filepaths)
+
+def from_different_machines(path1, path2) -> bool:
+    for s in DIRECTORIES_OF_INTEREST:
+        if s in path1:
+            source = s
+    return source not in path2
+
 
 results = []
 for apk in APKS_OF_INTEREST:
+    print(f"\n{apk} matched: ")
     for i in range(0, int(len(unique_bundle_filepaths))):
         for j in range(0, i):
             if i != j:
+                path1 = apk_filepath(unique_bundle_filepaths[i], apk)
+                path2 = apk_filepath(unique_bundle_filepaths[j], apk)
                 result = run_apkdiff(
-                    apk_filepath(unique_bundle_filepaths[i], apk),
-                    apk_filepath(unique_bundle_filepaths[j], apk),
+                    path1,
+                    path2,
                 )
                 # print(i, j, f" :{result}")
                 results.append(result)
-                if result:
+                if result and from_different_machines(path1, path2):
                     ### Spit out any result that matched in a readable form
                     print(
-                        f"{apk} matched: {get_folder_run_from_apk_filepath(unique_bundle_filepaths[i])} - {get_folder_run_from_apk_filepath(unique_bundle_filepaths[j])}"
+                        f"{get_folder_run_from_apk_filepath(unique_bundle_filepaths[i])} - {get_folder_run_from_apk_filepath(unique_bundle_filepaths[j])}"
                     )
