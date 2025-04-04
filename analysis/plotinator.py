a9bf0dd4d667e044b174476b286f3b0d8bf21543
Cerenia <christelle.gloor@inf.ethz.ch>
2025-04-04 07:03:12 +0000

    finally managed to create the correctly formatted dataframe

--- analysis/plotinator.py
+++ analysis/plotinator.py
@@ -19,13 +19,13 @@ def create_multiindex(index, hierarchy="version"):
     """
     hierarchy := "version"|"params"
     """
-    # For now, we assume that withever is not set by hierarchy will be constant
+    # For now, we assume that witchever is not set by hierarchy will be constant
     # Sanity check this
     nothierarchy = []
     for tarfile in index:
         (version, _ , _, dfs, ctime, reverse) = extract_parameters(tarfile)
         if hierarchy == "version":
-            nothierarchy.append(dfs, ctime, reverse)
+            nothierarchy.append((dfs, ctime, reverse))
         else:
             nothierarchy.append(version)
     assert(len(set(nothierarchy)) == 1), f"Unexpected {"parameters" if hierarchy == "version" else "versions"}: {set(nothierarchy)}"
@@ -34,26 +34,41 @@ def create_multiindex(index, hierarchy="version"):
     new_index = []
     for tarfile in index:
         (version, run, dfstest, dfs, ctime, reverse) = extract_parameters(tarfile)
-        complete_run = 
+        complete_run = f"t_{run}" if dfstest else run
         if hierarchy == "version":
-
-
+            new_index.append((version, complete_run))
+        else:
+            new_index.append((f"{'ctime' if ctime else 'alph'}_{'reversed' if reverse else 'sorted'}", complete_run))
+    names = ["version" if version else "parameters", "run"]
+    return pd.MultiIndex.from_tuples(new_index), names
 
 
 def visualize():
     with open(os.path.join(SUMMARY_ROOT, "dex_sort", "fixed_parameters","ctime_reverse_sorted.json"), 'r') as f:
         obj = json.loads(f.read())
-    #print(json.dumps(obj, indent=4))
-    # turn this into a meaningful dataframe
-    # first, sort the inner dicts
+    print(json.dumps(obj, indent=4))
+    # turn this into a dataframe which will later be copied into a new one with meaningful indices
     df = pd.DataFrame(data=obj)
-    df.replace({False:0, True:1}, inplace=True)
-    df.fillna(3, inplace=True)
-    #df = df.reindex(sorted(df.columns), axis=1)
-    #df = df.reindex(sorted(df.))
-    print(df.index)
+    # the matrix is symmetric
+    nr_of_labels = len(df.index)
+    new_index, names = create_multiindex(df.index)
+    new_column_labels, names = create_multiindex(df.columns)
+    print(new_index)
+    print(new_column_labels)
+    print(df)
+    df = pd.DataFrame(df.to_numpy(), index=new_index, columns=new_column_labels)
+    print(df)
+    exit(0)
+
+
+
     df.sort_index(axis=1, inplace=True)
     df.sort_index(inplace=True)
+    print(df)
+    df.replace({False:0, True:1}, inplace=True)
+    print(df)
+    df.fillna(3, inplace=True)
+    print(df)
     #df = df.reindex(sorted(df.rows), axis=0)
     #print(df)
     #mask = np.triu(np.ones_like(df, dtype=bool))
