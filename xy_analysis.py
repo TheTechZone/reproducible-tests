03b0800be6a067d708396dd3b54e0604a0e4c460
Cerenia <christelle.gloor@inf.ethz.ch>
2025-04-27 14:12:41 +0000

    take metadata list out of compare list

--- xy_analysis.py
+++ xy_analysis.py
@@ -26,6 +26,6 @@ def pretty_print_raw(filepath):
 
 # test()
 #run_all_checks([compare_dex_hashes, compare_first_dex_hash, compare_metadata_list], with_metadata_list=True)
-run_all_checks([compare_dex_hashes, compare_first_dex_hash, compare_metadata_list], with_metadata_list=True)
+run_all_checks([compare_dex_hashes, compare_first_dex_hash], with_metadata_list=True)
 pretty_print_raw("/home/chrissy/Code/reproducible-tests/data/summary/metadata_list/fixed_versions/7.30.2.json")
 visualize()
